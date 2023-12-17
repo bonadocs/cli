97f7774dbf3350060f22cda31ab5f2eb4ce9366c
staa99 <alfawwaz@mono.co>
2023-12-17 16:13:33 +0100

    Create structure for command execution

---
+++ src/lib/commands/collections/index.ts
@@ -0,0 +1,17 @@
+import { Argv } from 'yargs';
+export default {
+  command: 'collections <command>',
+  describe: 'Manage collections',
+  builder: (yargs: Argv) => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    yargs.command(require('./create').default);
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    yargs.command(require('./delete').default);
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    yargs.command(require('./list').default);
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    yargs.command(require('./rename').default);
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    yargs.command('*', ':id <command>', require('./[id]').default);
+  },
+};
---
+++ src/lib/commands/index.ts
@@ -0,0 +1,5 @@
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+yargs(hideBin(process.argv)).command(require('./collections').default).argv;
