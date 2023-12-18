fe0cb6babf29d7e1adb8be19df95efd6378fb6e7
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2023-12-18 18:40:26 +0100

    Implement stub for list collection command

---
+++ src/lib/runner/commands/collections/list.ts
@@ -0,0 +1,21 @@
+﻿import { getLocalCollections } from '../../../integrations/core'
+import { PromptOption, RoutedProcessorBase } from '../../util'
+import { CommonOptions } from '../types'
+
+export default class ListCollectionsCommandProcessor extends RoutedProcessorBase<CommonOptions> {
+  get options(): PromptOption[] {
+    return []
+  }
+
+  async process() {
+    const collections = await getLocalCollections()
+    console.log(
+      'Listing collection:\n',
+      collections.map((c, i) => `${i + 1}. ${c.name} (${c.id})`).join(`\n`),
+    )
+  }
+
+  get help() {
+    return 'List all collections on this device'
+  }
+}
