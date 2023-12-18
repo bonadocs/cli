353dfddc693ea845cf82be63cd806c876284afbf
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2023-12-18 03:44:33 +0100

    Delete old root

--- src/lib/commands/index.ts
+++
@@ -1,5 +0,0 @@
-import yargs from 'yargs'
-import { hideBin } from 'yargs/helpers'
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-yargs(hideBin(process.argv)).command(require('./collections').default).argv
