92c44dcf467432de7b0880d0f820cf1cc3dfc0a3
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2023-12-18 22:10:04 +0100

    Implement basic collection CRUD

--- src/lib/integrations/core/index.ts
+++ src/lib/integrations/core/index.ts
@@ -1,1 +1,47 @@
-﻿export * from './get-local-collections'
+﻿import {
+  Collection,
+  CollectionMetadataView,
+  deleteLocalCollectionName,
+  getCollectionStore,
+  getLocalCollectionNames,
+} from '@bonadocs/core'
+
+import { CollectionWithId } from './types'
+
+export async function getLocalCollections(): Promise<CollectionWithId[]> {
+  const collections = await getLocalCollectionNames()
+  return Object.entries(collections).map(([id, { name }]) => ({
+    id,
+    name,
+  }))
+}
+
+export async function createCollection(name: string, description: string) {
+  const manager = Collection.createBlankCollection(name, description).manager()
+  const metadataView = new CollectionMetadataView(manager)
+  await metadataView.rename(name)
+  return manager
+}
+
+export async function loadCollectionFromIPFS(uri: string) {
+  const collection = await Collection.createFromIPFS(uri)
+  return collection.manager()
+}
+
+export async function renameCollection(id: string, name: string) {
+  const store = await getCollectionStore(id)
+  const snapshot = await store.get('id')
+  if (!snapshot) {
+    throw new Error(`Collection ${id} not found`)
+  }
+
+  const manager = Collection.createFromSnapshot(snapshot).manager()
+  const metadataView = new CollectionMetadataView(manager)
+  await metadataView.rename(name)
+}
+
+export async function deleteCollection(id: string) {
+  const store = await getCollectionStore(id)
+  await store.remove(id)
+  deleteLocalCollectionName(id)
+}
