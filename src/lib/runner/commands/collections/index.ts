2bbea98a371091c79d211d8ed9a2158961e97ef5
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2023-12-18 03:47:48 +0100

    Create root collection command processor

---
+++ src/lib/runner/commands/collections/index.ts
@@ -0,0 +1,34 @@
+﻿import { RouterCommandProcessor } from '../../router'
+import { PromptOption, RouterCommandProcessorOptions } from '../../util'
+import { CommonOptions } from '../types'
+
+export default class RootCollectionCommandProcessor extends RouterCommandProcessor<CommonOptions> {
+  get options(): PromptOption[] {
+    return []
+  }
+
+  async process(options: RouterCommandProcessorOptions<CommonOptions>) {
+    try {
+      await super.process(options)
+    } catch {
+      console.log(this.help)
+    }
+  }
+
+  get help() {
+    return `Usage collections <command> [options]
+
+Commands:
+  create                          Create collection
+  delete                          Create collection
+  list                            Create collection
+  rename                          Create collection
+
+Options:
+  -I, --disable-interactivity     Disable interactivity
+
+Get options for a specific command with:
+  bonadocs <command> --help
+`
+  }
+}
