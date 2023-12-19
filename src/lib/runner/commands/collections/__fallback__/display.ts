cf22f59a89ac74e7194287774d2cd76a4173a2ba
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2023-12-20 00:59:53 +0100

    Stub display command

---
+++ src/lib/runner/commands/collections/__fallback__/display.ts
@@ -0,0 +1,37 @@
+﻿import { PromptOption } from '../../../util'
+import { CollectionOptions } from '../types'
+
+import { RoutedProcessorBase } from '#commands'
+
+type DisplayCollectionOptions = {
+  format: string
+} & CollectionOptions
+
+export default class DisplayCollectionCommandProcessor extends RoutedProcessorBase<
+  CollectionOptions,
+  DisplayCollectionOptions
+> {
+  get options(): PromptOption[] {
+    return [
+      {
+        name: 'format',
+        aliases: ['f'],
+        prompt: 'Format:',
+        type: 'string',
+        choices: ['json', 'json-minified'],
+        default: 'json',
+        required: true,
+      },
+    ]
+  }
+
+  async process(options: DisplayCollectionOptions) {
+    console.log(
+      `Displaying collection ${options.collectionId} using format ${options.format}`,
+    )
+  }
+
+  protected get commandDescription(): string {
+    return 'Create a new collection with the provided name and description.'
+  }
+}
