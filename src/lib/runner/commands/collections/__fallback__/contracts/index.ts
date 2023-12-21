8ee89902acd1e0d04ff74d9be224e31533dcd570
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2023-12-21 04:34:46 +0100

    Implement root contract command

---
+++ src/lib/runner/commands/collections/__fallback__/contracts/index.ts
@@ -0,0 +1,42 @@
+﻿import { CollectionOptions } from '../../types'
+
+import { RouterCommandProcessor, RouterCommandProcessorOptions } from '#router'
+
+export default class RootContractCommandProcessor extends RouterCommandProcessor<
+  RouterCommandProcessorOptions<CollectionOptions>
+> {
+  constructor(
+    contextOptions: RouterCommandProcessorOptions<CollectionOptions>,
+  ) {
+    super(contextOptions)
+  }
+
+  get options() {
+    return []
+  }
+
+  protected get commands() {
+    return [
+      {
+        name: 'list',
+        description: `List the contracts in collection '${this.contextOptions.collectionDataManager.metadataView.name}'`,
+      },
+      {
+        name: 'add',
+        description: `Add a contract to collection '${this.contextOptions.collectionDataManager.metadataView.name}'`,
+      },
+      {
+        name: 'delete',
+        description: `Delete a contract from '${this.contextOptions.collectionDataManager.metadataView.name}'`,
+      },
+      {
+        name: 'rename',
+        description: `Rename a contract in collection '${this.contextOptions.collectionDataManager.metadataView.name}'`,
+      },
+    ]
+  }
+
+  protected get commandDescription(): string {
+    return `Manage the contracts in collection '${this.contextOptions.collectionDataManager.metadataView.name}'`
+  }
+}
