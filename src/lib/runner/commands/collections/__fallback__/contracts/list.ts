4a5a804fd0696a9be51496382a64ddf2b9a6aa29
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2023-12-21 04:35:06 +0100

    Implement list contracts command

---
+++ src/lib/runner/commands/collections/__fallback__/contracts/list.ts
@@ -0,0 +1,23 @@
+﻿import { CollectionOptions } from '../../types'
+
+import { RoutedProcessorBase } from '#commands'
+
+export default class ListContractsCommandProcessor extends RoutedProcessorBase<CollectionOptions> {
+  get options() {
+    return []
+  }
+
+  async process() {
+    const contracts =
+      this.contextOptions.collectionDataManager.contractManagerView.contracts
+
+    console.log('Listing contracts:')
+    for (const contract of contracts) {
+      console.log(`${contract.name} (${contract.id})`)
+    }
+  }
+
+  protected get commandDescription(): string {
+    return 'List all collections on this device'
+  }
+}
