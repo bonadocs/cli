b33126063cf1abf73f73f2b93c1e5b533365e121
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2023-12-22 01:36:31 +0100

    Enable collection selection when name matches multiple collections

--- src/lib/runner/commands/collections/__fallback__/index.ts
+++ src/lib/runner/commands/collections/__fallback__/index.ts
@@ -1,7 +1,13 @@
-﻿import { CollectionOptions } from '../types'
+﻿import { CollectionDataManager } from '@bonadocs/core'
+
+import { CollectionOptions } from '../types'
 
 import { CommandDescription } from '#commands'
-import { getLocalCollections, loadCollectionById } from '#integrations/core'
+import {
+  CollectionWithId,
+  getLocalCollections,
+  loadCollectionById,
+} from '#integrations/core'
 import { RouterCommandProcessor, RouterCommandProcessorOptions } from '#router'
 
 export default class RootCollectionCommandProcessor extends RouterCommandProcessor<
@@ -15,30 +21,8 @@ export default class RootCollectionCommandProcessor extends RouterCommandProcess
 
   async setup(): Promise<void> {
     await super.setup()
-    const localCollections = await getLocalCollections()
-
-    const commandNameIndex =
-      this.contextOptions.commandStack.findIndex((c) => c === 'collections') + 1
-
-    if (commandNameIndex === 0) {
-      throw new Error(
-        '[FATAL] Invalid command stack. Expected collections command.',
-      )
-    }
-
-    const commandName = this.contextOptions.commandStack[commandNameIndex]
-    const collection = localCollections.find(
-      (collection) =>
-        collection.id.toLowerCase() === commandName.toLowerCase() ||
-        collection.name.toLowerCase().includes(commandName.toLowerCase()),
-    )
-
-    if (!collection) {
-      throw new Error(`Collection ${commandName} not found`)
-    }
-    this.contextOptions.collectionDataManager = await loadCollectionById(
-      collection.id,
-    )
+    this.contextOptions.collectionDataManager =
+      await this.getCollectionDataManager()
   }
 
   get options() {
@@ -63,6 +47,80 @@ export default class RootCollectionCommandProcessor extends RouterCommandProcess
   }
 
   protected get commandDescription(): string {
-    return `Manage collection '${this.contextOptions.collectionDataManager.metadataView.name}'`
+    return `Manage collection '${
+      this.contextOptions.collectionDataManager?.metadataView.name ||
+      '[not found]'
+    }'`
+  }
+
+  private async getCollectionDataManager(): Promise<CollectionDataManager> {
+    const localCollections = await getLocalCollections()
+
+    const commandNameIndex =
+      this.contextOptions.commandStack.findIndex((c) => c === 'collections') + 1
+
+    if (commandNameIndex === 0) {
+      throw new Error(
+        '[FATAL] Invalid command stack. Expected collections command.',
+      )
+    }
+
+    const commandName = this.contextOptions.commandStack[commandNameIndex]
+    const collection = await this.getCollectionFromCommandName(
+      localCollections,
+      commandName,
+    )
+    return loadCollectionById(collection.id)
+  }
+
+  private async getCollectionFromCommandName(
+    localCollections: CollectionWithId[],
+    commandName: string,
+  ) {
+    const collection = localCollections.find(
+      (collection) => collection.id.toLowerCase() === commandName.toLowerCase(),
+    )
+
+    if (collection) {
+      return collection
+    }
+
+    const filteredCollections = localCollections.filter((collection) =>
+      collection.name.toLowerCase().includes(commandName.toLowerCase()),
+    )
+
+    if (filteredCollections.length === 0) {
+      throw new Error(`Collection ${commandName} not found`)
+    }
+
+    if (filteredCollections.length === 1) {
+      return filteredCollections[0]
+    }
+
+    return this.promptForCollection(filteredCollections)
+  }
+
+  private async promptForCollection(filteredCollections: CollectionWithId[]) {
+    const answer = (await this.prompt([
+      {
+        name: 'collectionId',
+        aliases: ['c'],
+        type: 'string',
+        prompt: `Multiple collections match the provided name. Select collection:`,
+        required: true,
+        choices: filteredCollections.map((collection) => ({
+          name: `${collection.name} (${collection.id})`,
+          value: collection.id,
+        })),
+      },
+    ])) as { collectionId?: string }
+
+    if (!answer.collectionId) {
+      throw new Error('No collection selected')
+    }
+
+    return filteredCollections.find(
+      (collection) => collection.id === answer.collectionId,
+    )!
   }
 }
