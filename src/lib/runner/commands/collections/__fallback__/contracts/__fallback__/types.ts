f4a93563519f8b0dc2ac658642d8e415301c337c
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2023-12-22 02:09:38 +0100

    Create contract types

---
+++ src/lib/runner/commands/collections/__fallback__/contracts/__fallback__/types.ts
@@ -0,0 +1,7 @@
+﻿import { ContractDefinition } from '@bonadocs/core'
+
+import { CollectionOptions } from '../../types'
+
+export type ContractOptions = {
+  contract: ContractDefinition
+} & CollectionOptions
