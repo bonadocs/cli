e100b23fd1fc9e39614076f5804bf55c8f4ae89d
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2023-12-24 13:56:55 +0100

    Implement root registry command

---
+++ src/lib/runner/commands/registry/index.ts
@@ -0,0 +1,32 @@
+import { preIndexDataForSearchDB } from '@bonadocs/core'
+
+import { CommandDescription, CommonOptions } from '#commands'
+import { RouterCommandProcessor } from '#router'
+
+export default class RootRegistryCommandProcessor extends RouterCommandProcessor<CommonOptions> {
+  get options() {
+    return []
+  }
+
+  async setup(): Promise<void> {
+    await super.setup()
+    await preIndexDataForSearchDB()
+  }
+
+  protected get commands(): CommandDescription[] {
+    return [
+      {
+        name: 'register',
+        description: 'Register a protocol from a local collection',
+      },
+      {
+        name: 'search',
+        description: 'Search for a protocol in the registry',
+      },
+      {
+        name: 'open',
+        description: 'Download and open a protocol from the registry',
+      },
+    ]
+  }
+}
