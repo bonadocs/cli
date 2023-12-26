cb41b6efbe10f320ca5b38674ea82de780581373
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2023-12-26 06:16:56 +0100

    Fix multiple bugs in registerProtocol
    - Add a 10 second delay after fork creation before cloning
    - Delete fork if an error occurs
    - Delete previous existing fork before processing
    - Automatically create PR to registry repo

--- src/lib/integrations/registry/index.ts
+++ src/lib/integrations/registry/index.ts
@@ -3,7 +3,6 @@ import * as fs from 'fs/promises'
 import * as os from 'os'
 import * as path from 'path'
 
-import { Collection } from '@bonadocs/core'
 import axios, { AxiosInstance } from 'axios'
 
 type ProtocolMetadataLink = {
@@ -25,26 +24,59 @@ export type ProtocolMetadata = {
   links: ProtocolMetadataLink[]
 }
 
-let httpClient: AxiosInstance
+let httpClient: AxiosInstance | undefined
 
 const registryRepo = {
   owner: 'bonadocs',
   name: 'protocol-registry',
 }
 
+export async function getGitHubUsername(gitHubToken: string): Promise<string> {
+  initializeHttpClient(gitHubToken)
+  const response = await httpClient!.get('/user')
+  return response.data.login
+}
+
+export async function fetchTags(): Promise<string[]> {
+  const response = await axios.get(
+    `https://raw.githubusercontent.com/${registryRepo.owner}/${registryRepo.name}/main/tags.txt`,
+  )
+  return response.data
+    .split('\n')
+    .filter((s: string) => s.trim())
+    .map((s: string) => s.trim())
+}
+
 export async function registerProtocol(
   githubToken: string,
   protocol: ProtocolMetadata,
 ) {
+  console.log('Registering protocol...')
   await validateProtocolMetadata(protocol)
   initializeHttpClient(githubToken)
-
-  const random = Math.floor(Math.random() * 100000)
-  const forkName = `bonadocs-protocol-registry-${protocol.slug}-${random}`
-  const fork = await createFork(forkName)
-  const repoPath = await cloneFork(fork)
-  await updateFiles(protocol, repoPath)
-  commitAndPushChanges(protocol.name, repoPath)
+  const fork = await createFork(protocol.slug)
+  let repoPath: string | undefined
+  try {
+    console.log('Waiting 10 seconds for fork to be available for cloning...')
+    await new Promise((resolve) => setTimeout(resolve, 5000))
+    const repoPath = await cloneFork(fork)
+    await updateFiles(protocol, repoPath)
+    commitAndPushChanges(protocol.name, repoPath)
+    const username = await getGitHubUsername(githubToken)
+    await createPullRequest(username, protocol.name)
+  } catch (error) {
+    await deleteFork(fork)
+    console.error(
+      'Error registering protocol:',
+      error instanceof Error ? error.message : error,
+    )
+    throw error
+  } finally {
+    if (repoPath) {
+      await fs.rm(repoPath, { recursive: true, force: true })
+    }
+    console.log('Command done processing.')
+  }
 }
 
 async function tmpdir() {
@@ -54,12 +86,16 @@ async function tmpdir() {
   return dir
 }
 
-async function createFork(protocolSlug: string): Promise<string> {
+async function createFork(
+  protocolSlug: string,
+  fromDelete = false,
+): Promise<string> {
   try {
+    console.log('Creating fork...')
     const random = Math.floor(Math.random() * 100000)
     const forkName = `bonadocs-protocol-registry-${protocolSlug}-${random}`
 
-    const response = await httpClient.post(
+    const response = await httpClient!.post(
       `/repos/${registryRepo.owner}/${registryRepo.name}/forks`,
       {
         name: forkName,
@@ -68,8 +104,13 @@ async function createFork(protocolSlug: string): Promise<string> {
     )
 
     const forkedRepoFullName = response.data.full_name
-    console.log(`Fork created successfully: ${forkedRepoFullName}`)
-    return forkedRepoFullName
+    if (forkedRepoFullName && forkedRepoFullName.endsWith(forkName)) {
+      console.log(`Fork created successfully: ${forkedRepoFullName}`)
+      return forkedRepoFullName
+    } else if (forkedRepoFullName && !fromDelete) {
+      await deleteFork(forkedRepoFullName)
+      return createFork(protocolSlug, true)
+    }
   } catch (error) {
     console.error(
       'Error creating fork:',
@@ -77,15 +118,67 @@ async function createFork(protocolSlug: string): Promise<string> {
     )
     throw error
   }
+  throw new Error('Failed to create fork.')
+}
+
+async function deleteFork(fork: string): Promise<void> {
+  try {
+    const response = await httpClient!.delete(`/repos/${fork}`)
+    if (response.status === 204) {
+      console.log(`Fork deleted successfully: ${fork}`)
+      return
+    }
+  } catch (error) {
+    console.error(
+      'Error deleting fork:',
+      error instanceof Error ? error.message : error,
+    )
+    throw error
+  }
+  throw new Error(
+    'Failed to delete fork. Please delete it manually or provide a token with delete_repo permissions.',
+  )
+}
+
+async function createPullRequest(
+  githubUsername: string,
+  protocolName: string,
+): Promise<void> {
+  try {
+    console.log('Creating pull request...')
+    const response = await httpClient!.post(
+      `/repos/${registryRepo.owner}/${registryRepo.name}/pulls`,
+      {
+        title: `Add ${protocolName}`,
+        head: `${githubUsername}:main`,
+        base: 'main',
+      },
+    )
+
+    const pullRequestNumber = response.data.number
+    if (pullRequestNumber) {
+      console.log(`Pull request created successfully: ${pullRequestNumber}`)
+      return
+    }
+  } catch (error) {
+    console.error(
+      'Error creating pull request:',
+      error instanceof Error ? error.message : error,
+    )
+    throw error
+  }
+  throw new Error('Failed to create pull request')
 }
 
 async function cloneFork(fork: string): Promise<string> {
   try {
     // Clone the forked repository to a temporary folder
+    console.log('Cloning fork...')
     const tempRepoPath = await tmpdir()
     childProcess.execSync(
       `git clone https://github.com/${fork}.git ${tempRepoPath}`,
     )
+    console.log('Fork cloned successfully.')
     return tempRepoPath
   } catch (error) {
     console.error(
@@ -100,6 +193,7 @@ async function updateFiles(
   metadata: ProtocolMetadata,
   repoPath: string,
 ): Promise<void> {
+  console.log('Updating files...')
   const namesFilePath = path.join(repoPath, '/names.txt')
   const protocolSlug = metadata.slug
 
@@ -119,27 +213,40 @@ async function updateFiles(
   }
 
   // Write metadata to /data/[slug].json
+  console.log('Writing metadata...')
   const metadataFilePath = path.join(repoPath, `/data/${protocolSlug}.json`)
   await fs.writeFile(metadataFilePath, JSON.stringify(metadata, null, 2))
+  console.log('Metadata written successfully.')
+  console.log('Files updated successfully.')
 }
 
 async function appendName(file: string, slug: string, name: string) {
-  // validate slug
+  console.log('Appending name...')
   const content = await fs.readFile(file, 'utf-8')
   const slugs = content.split('\n').map((line) => line.split(':')[0].trim())
   if (slugs.includes(slug)) {
     throw new Error(`Slug ${slug} already exists`)
   }
   await fs.appendFile(file, `${slug}: ${name}\n`)
+  console.log('Name appended successfully.')
 }
 
 async function appendSlug(file: string, slug: string) {
-  const content = await fs.readFile(file, 'utf-8')
+  console.log(`Appending slug to ${file}...`)
+  let content: string
+  try {
+    content = await fs.readFile(file, 'utf-8')
+  } catch {
+    await fs.writeFile(file, '')
+    content = ''
+  }
+
   const slugs = content.split('\n').map((line) => line.trim())
   if (slugs.includes(slug)) {
     throw new Error(`Slug ${slug} already exists`)
   }
   await fs.appendFile(file, `${slug}\n`)
+  console.log('Slug appended successfully.')
 }
 
 async function validateProtocolMetadata(metadata: ProtocolMetadata) {
@@ -168,8 +275,6 @@ async function validateProtocolMetadata(metadata: ProtocolMetadata) {
     throw new Error('Protocol collection URI is required')
   }
   metadata.collection = metadata.collection.trim()
-
-  await Collection.createFromURI(metadata.collection)
 
   if (!metadata.owners?.trim()) {
     throw new Error(
@@ -250,6 +355,10 @@ async function validateProtocolMetadata(metadata: ProtocolMetadata) {
 }
 
 function initializeHttpClient(token: string) {
+  if (httpClient) {
+    return
+  }
+
   httpClient = axios.create({
     baseURL: 'https://api.github.com',
     headers: {
@@ -261,6 +370,7 @@ function initializeHttpClient(token: string) {
 }
 
 function commitAndPushChanges(protocolName: string, repoPath: string) {
+  console.log('Committing and pushing changes...')
   childProcess.execSync(
     `cd ${repoPath} && git add . && git commit -m "Add ${protocolName}" && git push origin main`,
   )
