3e6aa5f1ec799b1b548ba6d900b6cc9d54bb8d59
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2023-12-29 17:32:16 +0100

    Implement the deployments command
    Signed-off-by: Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>

---
+++ src/lib/runner/commands/collections/__fallback__/contracts/__fallback__/deployments.ts
@@ -0,0 +1,32 @@
+﻿import { supportedChains } from '@bonadocs/core'
+
+import { ContractOptions } from './types'
+
+import { RoutedProcessorBase } from '#commands'
+
+export default class ListDeploymentsCommandProcessor extends RoutedProcessorBase<ContractOptions> {
+  get options() {
+    return []
+  }
+
+  async process() {
+    const deployments = this.contextOptions.contract.instances
+
+    console.log('Deployments:')
+    console.log(
+      deployments
+        .map(
+          (instance) =>
+            `${
+              supportedChains.get(instance.chainId)?.name ||
+              `EVM Chain: ${instance.chainId}`
+            }: ${instance.address}`,
+        )
+        .join('\n'),
+    )
+  }
+
+  protected get commandDescription(): string {
+    return `List the deployments of the ${this.contextOptions.contract.name} contract across chains`
+  }
+}
