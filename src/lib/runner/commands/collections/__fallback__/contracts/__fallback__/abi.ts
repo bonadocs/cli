edcbeff8e294d8e5f35c695947f737591dc9fd98
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2023-12-29 17:25:34 +0100

    Implement the print ABI command
    Signed-off-by: Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>

---
+++ src/lib/runner/commands/collections/__fallback__/contracts/__fallback__/abi.ts
@@ -0,0 +1,24 @@
+﻿import { ContractOptions } from './types'
+
+import { RoutedProcessorBase } from '#commands'
+
+export default class PrintABICommandProcessor extends RoutedProcessorBase<ContractOptions> {
+  get options() {
+    return []
+  }
+
+  async process() {
+    const contractManagerView =
+      this.contextOptions.collectionDataManager.contractManagerView
+    const contract = contractManagerView.getContractInterface(
+      this.contextOptions.contract.interfaceHash,
+    )
+
+    console.log('ABI:')
+    console.log(contract.abi)
+  }
+
+  protected get commandDescription(): string {
+    return `Print the ABI of the ${this.contextOptions.contract.name} contract`
+  }
+}
