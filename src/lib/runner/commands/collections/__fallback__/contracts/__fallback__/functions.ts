52fa04e5e8f6b596423dea28a5832cf0da261add
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2023-12-29 17:41:43 +0100

    Implement the list functions command
    Signed-off-by: Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>

---
+++ src/lib/runner/commands/collections/__fallback__/contracts/__fallback__/functions.ts
@@ -0,0 +1,30 @@
+﻿import { ContractOptions } from './types'
+
+import { RoutedProcessorBase } from '#commands'
+
+export default class ListFunctionsCommandProcessor extends RoutedProcessorBase<ContractOptions> {
+  get options() {
+    return []
+  }
+
+  async process() {
+    const contractManagerView =
+      this.contextOptions.collectionDataManager.contractManagerView
+    const iface = contractManagerView.getInterface(
+      this.contextOptions.contract.interfaceHash,
+    )
+
+    if (!iface) {
+      throw new Error(`Contract interface not found`)
+    }
+
+    console.log('Functions:')
+    iface.forEachFunction((fn) => {
+      console.log(fn.format('full'))
+    })
+  }
+
+  protected get commandDescription(): string {
+    return `List the functions of the ${this.contextOptions.contract.name} contract`
+  }
+}
