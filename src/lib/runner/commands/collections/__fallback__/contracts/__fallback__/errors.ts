ef361fad67d9ec45643bdc5e59ec2861bbfb47b6
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2023-12-29 17:42:05 +0100

    Implement the list errors command
    Signed-off-by: Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>

---
+++ src/lib/runner/commands/collections/__fallback__/contracts/__fallback__/errors.ts
@@ -0,0 +1,30 @@
+﻿import { ContractOptions } from './types'
+
+import { RoutedProcessorBase } from '#commands'
+
+export default class ListErrorsCommandProcessor extends RoutedProcessorBase<ContractOptions> {
+  get options() {
+    return []
+  }
+
+  async process() {
+    const contractManagerView =
+      this.contextOptions.collectionDataManager.contractManagerView
+    const iface = contractManagerView.getInterface(
+      this.contextOptions.contract.interfaceHash,
+    )
+
+    if (!iface) {
+      throw new Error(`Contract interface not found`)
+    }
+
+    console.log('Errors:')
+    iface.forEachError((err) => {
+      console.log(err.format('full'))
+    })
+  }
+
+  protected get commandDescription(): string {
+    return `List the errors of the ${this.contextOptions.contract.name} contract`
+  }
+}
