2cc8faf830c8edeef57670f96996fd8eb756125d
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2023-12-29 17:41:52 +0100

    Implement the list events command
    Signed-off-by: Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>

---
+++ src/lib/runner/commands/collections/__fallback__/contracts/__fallback__/events.ts
@@ -0,0 +1,30 @@
+﻿import { ContractOptions } from './types'
+
+import { RoutedProcessorBase } from '#commands'
+
+export default class ListEventsCommandProcessor extends RoutedProcessorBase<ContractOptions> {
+  get options() {
+    return []
+  }
+
+  async process() {
+    const contractManagerView =
+      this.contextOptions.collectionDataManager.contractManagerView
+    const iface = contractManagerView.getInterface(
+      this.contextOptions.contract.interfaceHash,
+    )
+
+    if (!iface) {
+      throw new Error(`Contract interface not found`)
+    }
+
+    console.log('Events:')
+    iface.forEachEvent((evt) => {
+      console.log(evt.format('full'))
+    })
+  }
+
+  protected get commandDescription(): string {
+    return `List the events of the ${this.contextOptions.contract.name} contract`
+  }
+}
