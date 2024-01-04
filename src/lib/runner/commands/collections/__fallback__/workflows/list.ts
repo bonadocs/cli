7c13f9e4e5ffecb5c5e89db73087e310113a5caa
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2024-01-04 07:59:59 +0100

    Implement list workflows command
    Signed-off-by: Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>

---
+++ src/lib/runner/commands/collections/__fallback__/workflows/list.ts
@@ -0,0 +1,23 @@
+﻿import { CollectionOptions } from '../types'
+
+import { RoutedProcessorBase } from '#commands'
+
+export default class ListWorkflowsCommandProcessor extends RoutedProcessorBase<CollectionOptions> {
+  get options() {
+    return []
+  }
+
+  async process() {
+    const workflows =
+      this.contextOptions.collectionDataManager.workflowManagerView.workflows
+
+    console.log('Listing workflows:')
+    for (const workflow of workflows) {
+      console.log(`${workflow.name} (${workflow.id})`)
+    }
+  }
+
+  protected get commandDescription(): string {
+    return 'List all workflows in this collection'
+  }
+}
